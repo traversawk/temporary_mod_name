fd60d11745c659813e1768b6d5ea9f8749325db4
JackTraversa <traversawk@gmail.com>
2025-06-15 08:24:13 -0400

    added in a 'crab' item complete with a (poorly drawn) texture

---
+++ src/main/java/net/pixeljj/tutorialmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.pixeljj.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.pixeljj.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final Item CRAB = registerItem("crab", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(CRAB);
+        });
+    }
+}
